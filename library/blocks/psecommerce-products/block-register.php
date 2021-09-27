333835598c8cc1266e578aac6cd2cc08c09550cd
Dernerd <dern3rd@outlook.at>
2021-09-27 02:06:33 +0200

    -plugin um Platz zu sparen

--- library/blocks/psecommerce-products/block-register.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( class_exists('PSeCommerce')){
-$class_file = __DIR__ . '/psecommerce-products-block.php';
-$icons = array(
-	'path' => __DIR__ . '/',
-	'url' => upfront_url() . '/library/blocks/psecommerce-products'
-);
-upfront_register_block('UpFrontPSeCommerceProductsBlock', upfront_url() . '/library/blocks/psecommerce-products', $class_file, $icons);
-}
--- library/common/plugins.php
+++ library/common/plugins.php
@@ -35,13 +35,13 @@ class UpFrontPlugins{
 			array(
 				'name'               => 'UpFront Shortcodes Plugin', 
 				'slug'               => 'upfront-shortcodes', 
-				'source'             => get_template_directory() . '/plugins/upfront-shortcodes.zip', 
+				'source'             => 'https://github.com/piestingtal-source/upfront-shortcodes/releases/download/1.0.7/upfront-shortcodes.zip', 
 				'required'           => false, 
-				'version'            => '1.0.2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+				'version'            => '1.0.7', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 				//'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-				'is_callable'        => true, // If set, this callable will be be checked for availability to determine if a plugin is active.
+				'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 					),
 			);
 
